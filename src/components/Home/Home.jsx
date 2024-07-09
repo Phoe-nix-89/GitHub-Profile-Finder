2369e036698b6eb0cc8d13ecb28ad544930bb411
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-07-09 23:20:08 +0530

    Tailwinf add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,10 @@ function Home() {
 
     return (
         <div className="flex flex-col items-center justify-between">
-            <input type="text" placeholder="Enter the name" onChange={(e) => {
+            <input type="text" placeholder="Enter the name" className="h-8 w-56 pl-2 m-4"  onChange={(e) => {
                 setSearchTerm(e.target.value);
             }} />
-            {searchTerm ? <GithubCardsContainer search={searchTerm} /> : <div className="font-bold">Write the name of the person to find</div>}
+            {searchTerm ? <GithubCardsContainer search={searchTerm} /> : <div className="font-bold text-2xl" >Write the Username of the person to find</div>}
         </div>
     );
 }
